c87e33f589beb48eb04111e892394e32038d864c
Maite Becker <beckermaite21@gmail.com>
2024-07-16 06:32:58 -0300

    Implementação da camada de dominio

---
+++ RestAPI/src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,33 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String icon;
+	private String desription;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public String getDesription() {
+		return desription;
+	}
+	public void setDesription(String desription) {
+		this.desription = desription;
+	}
+}
